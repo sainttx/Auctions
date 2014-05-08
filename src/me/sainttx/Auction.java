df9939b8f6ef30a5c271235263588cf893db421f
Matthew Steglinski <stteg@hotmail.com>
2014-05-07 20:27:20 -0400

    Adds beginnings of /auction start, /auction bid, and /auction end

--- src/me/sainttx/Auction.java
+++ src/me/sainttx/Auction.java
@@ -4,10 +4,13 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.Iterator;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Auction extends JavaPlugin {
@@ -15,18 +18,17 @@ public class Auction extends JavaPlugin {
 	private YamlConfiguration messages;
 	private YamlConfiguration log;
 	private IAuction auction;
-	
-	private boolean logauctions;
-	private boolean allowautowin;
-	private boolean allowcancel;
-	private boolean allowcreative;
-	private int timebetween;
-	private int bidincrease;
-	private int mintime;
-	private int maxtime;
-	private int cost;
-	private int percent;
-	
+
+	//	private boolean logauctions;
+	//	private boolean allowautowin;
+	//	private boolean allowcancel;
+	//	private boolean allowcreative;
+	//	private int timebetween;
+	//	private int bidincrease;
+	//	private int auctiontime;
+	//	private int cost;
+	//	private int percent;
+
 	/*
 	 * commands:
 	 * auction info - show info about current auction
@@ -34,73 +36,157 @@ public class Auction extends JavaPlugin {
 	 * auction end - end an auction
 	 * auction quiet - silence auction messages
 	 */
-	
+
 	@Override
 	public void onEnable() {
 		this.saveDefaultConfig();
 		loadConfig();
 		getCommand("auction").setExecutor(this);
 	}
-	
+
 	private void loadConfig() {
-		logauctions = getConfig().getBoolean("log-auctions");
-		allowautowin = getConfig().getBoolean("allow-autowin");
-		allowcancel = getConfig().getBoolean("allow-cancel");
-		allowcreative = getConfig().getBoolean("allow-creative");
-		timebetween = getConfig().getInt("time-between-bidding");
-		bidincrease = getConfig().getInt("minimum-bid-increment");
-		mintime = getConfig().getInt("minimum-auction-time");
-		maxtime = getConfig().getInt("maximum-auction-time");
-		cost = getConfig().getInt("auction-start-fee");
-		percent = getConfig().getInt("auction-tax-percentage");
+		//		logauctions = getConfig().getBoolean("log-auctions");
+		//		allowautowin = getConfig().getBoolean("allow-autowin");
+		//		allowcancel = getConfig().getBoolean("allow-cancel");
+		//		allowcreative = getConfig().getBoolean("allow-creative");
+		//		timebetween = getConfig().getInt("time-between-bidding");
+		//		bidincrease = getConfig().getInt("minimum-bid-increment");
+		//		auctiontime = getConfig().getInt("auction-time");
+		//		cost = getConfig().getInt("auction-start-fee");
+		//		percent = getConfig().getInt("auction-tax-percentage");
 		File messages = new File(getDataFolder(), "messages.yml");
 		if (!messages.exists()) {
 			saveResource("messages.yml", false);
 		}
 		this.messages = YamlConfiguration.loadConfiguration(messages);
 	}
-	
+
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		String username = sender.getName();
 		if (args.length == 0) {
-			for (Iterator<String> info = messages.getStringList("auction-menu").iterator(); info.hasNext();) {
-				sender.sendMessage(format(info.next()));
-			}
+			sendMenu(sender);
 		} else {
 			String arg1 = args[0];
 			if (arg1.equals("start")) {
 				if (ignoring.contains(username)) {
-					sender.sendMessage(format(messages.getString("fail-start-ignoring")));
+					sender.sendMessage(getMessageFormatted("fail-start-ignoring"));
 					return false;
 				} else {
-					
+					startAuction(sender, args); // auction start <amt> <min-bid> [autowin]
 				}
 			} else if (arg1.equals("end")) {
-				
+				if (this.auction == null) {
+					sender.sendMessage(getMessageFormatted("fail-end-no-auction"));
+					return false;
+				} else {
+					auction.end();
+					stopAuction();
+				}
 			} else if (arg1.equals("info")) {
-				
+
+			} else if (arg1.equals("bid")) {
+				if (auction != null) {
+					if (args.length == 2) {
+						auction.bid((Player) sender, Integer.parseInt(args[1])); // could throw
+					} else {
+						sender.sendMessage(getMessageFormatted("fail-bid-syntax"));
+					}
+				} else {
+					sender.sendMessage(getMessageFormatted("fail-bid-no-auction"));
+				}
 			} else if (arg1.equals("quiet") || arg1.equals("ignore")) {
 				if (!ignoring.contains(sender.getName())) {
-					sender.sendMessage(format(messages.getString("ignoring-on")));
+					sender.sendMessage(getMessageFormatted("ignoring-on"));
 					ignoring.add(sender.getName());
 				} else {
-					sender.sendMessage(format(messages.getString("ignoring-off")));
+					sender.sendMessage(getMessageFormatted("ignoring-off"));
 					ignoring.remove(sender.getName());
 				}
 			} else if (arg1.equals("reload")) {
 				if (!sender.hasPermission("auction.reload")) {
-					sender.sendMessage(format(messages.getString("insufficient-permissions")));
+					sender.sendMessage(getMessageFormatted("insufficient-permissions"));
 					return false;
 				}
 				reloadConfig();
 				loadConfig();
+			} else if (arg1.equals("test")) {
+				Player player = (Player) sender;
+				player.getItemInHand().setAmount(5);
+			} else if (arg1.equals("test1")) {
+				Player player = (Player) sender;
+				player.getItemInHand().setType(Material.AIR);
 			}
-				
+			else {
+				// invalid arg
+				sendMenu(sender);
+			}
+
 		}
 		return false;
 	}
-	
+
+	public void messageListening(String message) {
+		String message1 = messages.getString(message);
+		/* message.replaceAll("%i", "")
+			   	.replaceAll("%t", Integer.toString(auction.getTimeRemaining())
+				.replaceAll("", "")); */
+		for (Player player : Bukkit.getOnlinePlayers()) {
+			if (!ignoring.contains(player.getName())) {
+				player.sendMessage(format(message1));
+			}
+		}
+	}
+
+	public YamlConfiguration getMessages() {
+		return this.messages;
+	}
+
+	private void sendMenu(CommandSender sender) {
+		for (Iterator<String> info = messages.getStringList("auction-menu").iterator(); info.hasNext();) {
+			sender.sendMessage(format(info.next()));
+		}
+	}
+
+	private void startAuction(CommandSender sender, String[] args) {
+		if (this.auction != null) {
+			sender.sendMessage(getMessageFormatted("fail-start-auction-in-progress"));
+			return;
+		}
+		if (sender instanceof Player) {
+			if (args.length > 2) {
+				try {
+					int amount = Integer.parseInt(args[1]);
+					int start = Integer.parseInt(args[2]);
+					if (args.length == 4) { // auction start amount startingbid autowin
+						int autowin = Integer.parseInt(args[3]);
+						this.auction = new IAuction(this, (Player) sender, amount, start, autowin);
+						this.auction.start();
+						return;
+					}
+					this.auction = new IAuction(this, (Player) sender, amount, start, -1);
+					this.auction.start();
+				} catch (NumberFormatException ex1) {
+					sender.sendMessage(getMessageFormatted("fail-number-format"));
+				} catch (InsufficientItemsException ex2) {
+					sender.sendMessage(getMessageFormatted("fail-start-not-enough-items"));	
+				}
+			} else {
+				sender.sendMessage(getMessageFormatted("fail-start-syntax"));
+			}
+		} else {
+			sender.sendMessage(messages.getString("fail-console"));
+		}
+	}
+
+	public void stopAuction() {
+		this.auction = null;
+	}
+
+	public String getMessageFormatted(String message) {
+		return format(messages.getString(message));
+	}
+
 	private String format(String s) {
 		return ChatColor.translateAlternateColorCodes('&', s);
 	}
--- src/me/sainttx/IAuction.java
+++ src/me/sainttx/IAuction.java
@@ -1,27 +1,169 @@
 package me.sainttx;
 
+import java.util.UUID;
+
+import net.minecraft.server.v1_7_R3.Material;
+
+import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
 public class IAuction {
-	private String owner;
+	private Auction plugin;
+	private UUID owner;
 	private int startingAmount;
 	private int numItems;
+	private int autoWin;
+	private int timeRemaining;
+	private int taskID;
+	private int timeLeft;
+	private int increment;
 	private ItemStack item;
 
-	public IAuction(Player player, int numItems, int startingAmount) {
+	private UUID winning;
+	private int winningAmt;
+
+	private final int[] times = {45, 30, 10, 3, 2, 1};
+
+	public IAuction(Auction plugin, Player player, int numItems, int startingAmount, int autoWin) throws InsufficientItemsException {
+		this.plugin = plugin;
 		this.startingAmount = startingAmount;
 		this.numItems = numItems;
-		owner = player.getName();
-		item = player.getItemInHand();
-		item.setAmount(1);
+		this.autoWin = autoWin;
+		owner = player.getUniqueId();
+		item = player.getItemInHand().clone();
+		increment = plugin.getConfig().getInt("minimum-bid-increment");
+
+		timeLeft = Integer.parseInt(plugin.getConfig().getString("auction-time")); // could throw on invalid
+
+		if (searchInventory(player)) { // Check if they have enough
+			takeItems(player);
+		} else {
+			// Doesn't have enough of the item
+			throw new InsufficientItemsException();
+		}
 	}
 
-	public String getOwner() {
+	public void start() {
+		plugin.messageListening("auction-start");
+		plugin.messageListening("auction-start-price");
+		Runnable task = new Runnable() {
+			@Override
+			public void run() {
+				if (timeLeft <= 0) {
+					end();
+				} else {
+					--timeLeft;
+					for (int i : times) {
+						if (i == timeLeft) {
+							plugin.messageListening("auction-timer");
+						}
+					}
+				}
+			}
+		};
+		taskID = Bukkit.getScheduler().scheduleSyncRepeatingTask(plugin, task, 0L, 20L);
+	}
+
+	public void bid(Player player, int amount) {
+		if (amount <= winningAmt + increment) {
+			player.sendMessage(plugin.getMessageFormatted("fail-bid-too-low"));
+		} else if (winning.equals(player.getName())) {
+			player.sendMessage(plugin.getMessageFormatted("fail-bid-top-bidder"));
+		} else if (owner.equals(player.getName())) {
+			player.sendMessage(plugin.getMessageFormatted("fail-bid-your-auction"));
+		} else {
+			// bid here
+			if (amount >= autoWin && autoWin != -1) {
+				// They win
+				// Take away money
+				plugin.messageListening("auction-ended-autowin");
+				winning = player.getUniqueId();
+				end();
+			}
+			plugin.messageListening("bid-broadcast");
+		}
+	}
+
+	public boolean end() {
+		Bukkit.getScheduler().cancelTask(taskID);
+		if (winning == null) {
+			plugin.messageListening("auction-end-no-bidders");
+			plugin.stopAuction();
+			// Return items to owner
+			return true;
+		}	
+		OfflinePlayer winner = Bukkit.getOfflinePlayer(winning);
+		OfflinePlayer owner = Bukkit.getOfflinePlayer(this.owner);
+		if (winner.isOnline()) {
+			Player winner1 = (Player) winner;
+			winner1.sendMessage(plugin.getMessageFormatted("auction-winner"));
+			winner1.sendMessage(plugin.getMessageFormatted("auction-end-tax"));
+			// Give the items to the winner
+		} else {
+
+		}
+
+		if (owner.isOnline()) {
+
+		} else {
+
+		}
+
+		plugin.stopAuction();
+		return true;
+	}
+
+	private boolean searchInventory(Player player) {
+		int count = 0;
+		for (ItemStack is : player.getInventory()) {
+			if (is != null) { 
+				if (is.isSimilar(item)) {
+					if (is.getAmount() >= numItems) {
+						return true;
+					} else {
+						count += is.getAmount();
+					}
+				}
+			}
+		}
+		if (count >= numItems) { 
+			return true;
+		}
+		return false;
+	}
+
+	private void takeItems(Player player) {
+		Inventory playerInventory = player.getInventory();
+		this.item.setAmount(numItems);
+		int count = 0;
+		for (ItemStack is : player.getInventory()) { // .all()
+			if (is.isSimilar(item)) {
+				int size = is.getAmount();
+				if (size > numItems - count) {
+					is.setAmount(size - (numItems - count));
+					count = numItems;
+				} else if(size == numItems - count) {
+					count += size;
+					playerInventory.removeItem(new ItemStack(item));
+				} else {
+					count += size;
+					playerInventory.removeItem(new ItemStack(item));
+				}
+			}
+			if (count == numItems) {
+				break;
+			}
+		}
+	}
+
+	public UUID getOwner() {
 		return owner;
 	}
 
-	public void setOwner(String owner) {
+	public void setOwner(UUID owner) {
 		this.owner = owner;
 	}
 
@@ -48,4 +190,12 @@ public class IAuction {
 	public void setItem(ItemStack item) {
 		this.item = item;
 	}
+
+	public int getTimeRemaining() {
+		return timeLeft;
+	}
+}
+
+class InsufficientItemsException extends Exception {
+	
 }
