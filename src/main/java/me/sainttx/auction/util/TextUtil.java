f189e73d59645661b714e415d287d53794f962b3
Matthew Steglinski <matthew.steglinski@utoronto.ca>
2015-04-04 14:30:17 -0400

    * Revert 'copy fanciful files into the project rather then shading' * Update shaded fanciful to 0.3.3-SNAPSHOT * Supports spigot versions 1649, 1.8, 1.8.3

--- src/main/java/me/sainttx/auction/util/TextUtil.java
+++ src/main/java/me/sainttx/auction/util/TextUtil.java
@@ -3,7 +3,7 @@ package me.sainttx.auction.util;
 import me.sainttx.auction.Auction;
 import me.sainttx.auction.AuctionManager;
 import me.sainttx.auction.AuctionPlugin;
-import me.sainttx.auction.fanciful.FancyMessage;
+import mkremins.fanciful.FancyMessage;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
